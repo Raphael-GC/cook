f8740b901f72796cc0874d1b01f2bf827e4633cd
Raphael-GC <raphaelcarneirodev@gmail.com>
2024-03-04 23:54:39 -0300

    Initial commit

---
+++ assets/src/app/index/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native"
+
+export default function Index(){ /*When we're using Expo Router, we SHOULD set this function as a DEFAULT function*/
+    return (
+        <View>
+            <Text>React Native working</Text>
+        </View>
+    )
+} 
